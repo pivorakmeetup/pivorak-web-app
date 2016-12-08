9a9b23d45bebe6aa6c258b7f3b6fcc9d2f31842e
Denys Medynskyi <skrypalyk@users.noreply.github.com>
2016-12-08 22:15:30 +0200

    #55 User first_name & last_name validation (#59)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  LATIN_LETTERS_REGEX = /[a-zA-Z]+/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable, omniauth_providers: Devise.omniauth_providers
 
   has_many :identities, dependent: :destroy
   has_many :donations,  dependent: :destroy
+
+  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, format: { with: LATIN_LETTERS_REGEX,  message: I18n.t('errors.only_latin_letters') }
 
   def full_name
     first_name + ' ' + last_name
