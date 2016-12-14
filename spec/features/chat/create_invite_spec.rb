c211cbbc19dd93179a4d4c64376c56f3ee85757e
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-12-14 09:27:02 +0200

    mock Slack API calls

--- spec/features/chat/create_invite_spec.rb
+++ spec/features/chat/create_invite_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Chat features CREATE INVITE', vcr: { record: :new_episodes } do
+RSpec.describe 'Chat features CREATE INVITE' do
   before do
     visit '/chat'
   end
@@ -6,6 +6,10 @@ RSpec.describe 'Chat features CREATE INVITE', vcr: { record: :new_episodes } do
   let(:email) { 'fake@mail.com' }
 
   context 'when valid input' do
+    before do
+      expect(Chat::Invite).to receive(:call).with(email) { true }
+    end
+
     it 'sends invite' do
       fill_in 'email',  with: email
       click_button 'Get My Invite'
@@ -24,6 +28,10 @@ RSpec.describe 'Chat features CREATE INVITE', vcr: { record: :new_episodes } do
   end
 
   context 'when email already joined chat' do
+    before do
+      allow(Chat::Invite).to receive(:call).and_raise(Chat::Client::ChatError.new(I18n.t('chat.errors.already_in_team')))
+    end
+
     it 'shows error message' do
       fill_in 'email',  with: 'denys.medynskyi@gmail.com'
       click_button 'Get My Invite'
