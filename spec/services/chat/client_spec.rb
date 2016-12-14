a5b400ce5b3591e677ef917d668362719678483a
Denys Medynskyi <skrypalyk@users.noreply.github.com>
2016-12-14 09:32:02 +0200

    #53 Require first_name & last_name during registration (#60)
    * #53 Require first_name & last_name during registration
    
    * remove spec/cassettes from gitignore
    
    * regerate spec/cassettes
    
    * mock Slack API calls

---
+++ spec/services/chat/client_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Chat::Client do
+  let(:url) { Faker::Internet.url }
+  let(:response) { double('response', body: body) }
+  before do
+    expect_any_instance_of(HttpClient).to receive(:get).with(url, { token: ENV['SLACK_TOKEN'] }, {}) { response }
+  end
+  let(:client) { described_class.new(url: url) }
+
+  describe '#call' do
+    context 'when call is succesful' do
+      let(:body) { {'ok' => true}.to_json }
+      it { expect(client.call).to be_truthy }
+    end
+
+    context 'when call is failure' do
+      let(:error) { 'auth_error' }
+      let(:body) { {'error' => error}.to_json }
+      it { expect{ client.call }.to raise_error(Chat::Client::ChatError) }
+    end
+  end
+end
--- spec/services/chat/invite_spec.rb
+++ spec/services/chat/invite_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
-describe Chat::Invite, vcr: { record: :new_episodes } do
+describe Chat::Invite do
   let(:email) { 'new-user@mail.com' }
+  let(:mocked_client) { double('Chat::Client') }
   subject { described_class.new(email) }
 
   describe '#call' do
     context 'when success' do
+      before do
+        expect(Chat::Client).to receive(:new).with({:url=> Chat::Invite::INVITE_URL, :params=>{:resend=>true, :email=> email}}) { mocked_client }
+        expect(mocked_client).to receive(:call) { true }
+      end
       it { expect(subject.call).to be_truthy }
     end
 
     context 'when empty email' do
       it { expect(described_class.call('')).to be_falsey }
-    end
-
-    context 'when invited second time' do
-      it { expect {subject.call}.to raise_error(Chat::Client::ChatError, 'sent_recently') }
     end
   end
 end
