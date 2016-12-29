31eeb114baa6729526e1154c763481246a8b00b1
Volodya Sveredyuk <sveredyuk@gmail.com>
2016-12-29 16:00:13 +0200

    #95 Groupable polymorphic resource (#111)

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,6 +4,7 @@ class Talk < ApplicationRecord
 
   belongs_to :event
   belongs_to :speaker, class_name: 'User'
+  belongs_to :group
 
   validates :title, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@ User.where(email: 'user@mail.com').first_or_create!(
     first_name: 'User', last_name: 'Mail', password: '123456'
 ).confirm
 
+#=== Groups ====================================================================
+[:regular, :lighting, :workshop].each do |talk_type|
+  Group.where(resource: Talk, name: talk_type).first_or_create!
+end
