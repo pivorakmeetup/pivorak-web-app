cc209ac83abbde2506dfcf515bb811ec57376a70
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-12-29 16:01:55 +0200

    remove default_url from uploader because it is not used

--- app/uploaders/event_cover_uploader.rb
+++ app/uploaders/event_cover_uploader.rb
@@ -14,13 +14,6 @@ class EventCoverUploader < CarrierWave::Uploader::Base
     "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
-    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
-
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
