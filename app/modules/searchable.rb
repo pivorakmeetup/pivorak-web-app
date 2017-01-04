0bad6ce4fa06a3825d244525a2916a424402bc57
kushnir.yb <kushnir.yb@gmail.com>
2017-01-04 19:20:42 +0200

    #48 Searchable pluggable interface (#132)

---
+++ app/modules/searchable.rb
@@ -0,0 +1,17 @@
+module Searchable
+  class << self
+    def included(base)
+      base.include(search_module)
+
+      resolver.call(target: base)
+    end
+
+    def search_module
+      PgSearch
+    end
+
+    def resolver
+      ::Searchable::Resolver
+    end
+  end
+end
---
+++ spec/modules/searchable/configuration/event_spec.rb
@@ -0,0 +1,5 @@
+require 'modules/searchable/configuration/shared_group'
+
+RSpec.describe ::Searchable::Configuration::Event do
+  it_behaves_like 'Configurable'
+end
