2a405fa30f0d7ea1551d6407ed2919f78e099b63
Denys Medynskyi <denysmedynskyi@users.noreply.github.com>
2017-01-14 11:17:50 +0200

    #92 add talks to member page (#136)

--- spec/features/members/read_spec.rb
+++ spec/features/members/read_spec.rb
@@ -21,4 +21,28 @@ RSpec.describe 'Member READ' do
 
     it { click_link('Tester User') }
   end
+
+  describe 'member talks' do
+    context 'when user had talks' do
+      it 'shows member talks' do
+        user = create(:user)
+        talk = create(:talk, speaker: user)
+
+        visit "/members/#{user.id}"
+
+        expect(page).to have_link(talk.title, "talks/#{talk.slug}")
+      end
+    end
+
+    context 'when talk does not belong to user' do
+      it 'shows member talks' do
+        user = create(:user)
+        talk = create(:talk)
+
+        visit "/members/#{user.id}"
+
+        expect(page).to_not have_link(talk.title, "talks/#{talk.slug}")
+      end
+    end
+  end
 end
