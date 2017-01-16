7ceb9e51716f393daf23ee167ab5289776f43467
Denys Medynskyi <denysmedynskyi@users.noreply.github.com>
2017-01-16 18:57:02 +0200

    #97 Visited events at member's page (#140)

---
+++ app/finders/user/visited_events.rb
@@ -0,0 +1,15 @@
+class User
+  class VisitedEvents < ApplicationFinder
+    def initialize(params = {})
+      @user_id = params[:user_id]
+    end
+
+    def call
+      Event.joins(:visit_requests).where(visit_requests: { visited: true, user_id: user_id })
+    end
+
+    private
+
+    attr_reader :user_id    
+  end
+end
