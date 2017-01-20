d2f0c6473efaf060e838453fb8274d1f42314683
Olexandr Molodchenko <oleksandr@innocode.no>
2017-01-20 11:28:27 +0200

    add user for production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server '188.166.8.111', roles: %w{app db web}, user: 'pivorak'
 set :branch, 'development'
 set :deploy_to, proc { "/home/pivorak/projects/pivorak_#{fetch :stage}" }
+set :user, 'pivorak'
