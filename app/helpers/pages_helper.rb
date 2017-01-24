b6e5b6030bf1f678cb7ab1df0ce66dc417cbd3b2
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-01-24 07:32:12 +0200

    Refactor page_link helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,7 +2,7 @@ module PagesHelper
   def page_link(page, options = {})
     return unless page.persisted?
 
-    link_to (options[:text] || page.title),
+    link_to options.fetch(:text, page.title),
       "/#{page.url}", options.merge(target: '_blank')
   end
 end
