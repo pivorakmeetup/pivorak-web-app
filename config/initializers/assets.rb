50774884496be6cec15c28475ae29c409c3e6b25
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-02-02 10:23:07 +0200

    Fix assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( app/coming_soon.scss )
+Rails.application.config.assets.precompile += %w( app/coming_soon.css )
