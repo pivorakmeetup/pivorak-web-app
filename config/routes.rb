ccd4ff684c25cbc70e75967220903e405b265d44
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-02-02 10:02:20 +0200

    #164 Coming Soon Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   }
 
   #=== MAIN APP =================================
+  # TODO VS -> Remove after release
+  get '/coming-soon', to: 'home#coming_soon'
+
   resources :events,  only: %i[index show] do
     resources :visit_requests, only: %i[show create destroy]
   end
