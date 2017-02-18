615746bf6129d56b9eb1843cf4a507eed42e96b0
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-02-18 09:37:37 +0200

    Lock Cap at 3.7.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.7.1"
+lock "3.7.2"
 
 set :application, proc { "pivorak-web-app_#{fetch :stage}" }
 set :repo_url, 'git@github.com:pivorakmeetup/pivorak-web-app.git'
