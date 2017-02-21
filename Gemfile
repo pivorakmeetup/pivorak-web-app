e63b767148af682b84c292f1518c47907880f988
Denys Medynskyi <denys.medynskyi@gmail.com>
2017-02-21 11:30:14 +0200

    Replace `mailgun_rails` gem with plain SMTP

--- Gemfile
+++ Gemfile
@@ -31,7 +31,6 @@ gem 'kaminari',                         '~> 0.17'
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'httpclient'
 gem 'oj' # faster json parser
-gem 'mailgun_rails'
 gem 'rollbar'
 gem 'uglifier'
 # sidekiq
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-      api_key: ENV['MAILGUN_API_KEY'],
-      domain: ENV['MAILGUN_DOMAIN']
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => ENV['MAILGUN_DOMAIN'],
+    :user_name => ENV['MAILGUN_USER_NAME'],
+    :password => ENV['MAILGUN_PASSWORD']
   }
 
 end
