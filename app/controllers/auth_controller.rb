c05b7b25e020c87f3b126374ab3bce087de90dd3
oookli <oklika@gmail.com>
2017-02-26 20:51:23 +0200

    temp commit

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,3 @@
+class AuthController < ActionController::Base
+  layout "devise"
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,4 +273,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  config.parent_controller = 'AuthController'
 end
