9ac7925ecd1265dcc0aa67bbad12e11cc4e27e65
Denys Medynskyi <denysmedynskyi@users.noreply.github.com>
2017-03-10 13:00:58 +0200

    We need only one params resolver (#224)

--- spec/services/omniauth/authentication_service_spec.rb
+++ spec/services/omniauth/authentication_service_spec.rb
@@ -85,6 +85,33 @@ RSpec.describe ::Omniauth::AuthenticationService do
         expect(identity.provider).to eq params[:provider].to_s
       end
     end
+
+    context 'when github params' do
+      let(:params) { build(:github_params) }
+      subject      { described_class.new(params) }
+
+      it 'returns a valid instance of User class' do
+        expect(result).to be_an_instance_of User
+        expect(result.valid?).to eq true
+      end
+
+      it 'assigns email, first_name, last_name and password to the instance' do
+        expect(result.email).to eq params[:info][:email]
+        expect(result.first_name).to be_present
+        expect(result.last_name).to be_present
+        expect(result.encrypted_password).to be_present
+      end
+
+      it 'links identity to the instance' do
+        expect(identity).not_to be_nil
+        expect(identity.user).to eq result
+      end
+
+      it 'assigns uid and provider attributes to the identity' do
+        expect(identity.uid).to eq params[:uid]
+        expect(identity.provider).to eq params[:provider].to_s
+      end
+    end
   end
   context 'with invalid params' do
     subject { described_class.new({}) }
