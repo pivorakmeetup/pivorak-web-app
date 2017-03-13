bdc14266950b7096f3d372ac27c4a42fe39296e1
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-03-13 17:49:20 +0200

    Reset confirmation email shared link

--- spec/features/home/coming_soon_spec.rb
+++ spec/features/home/coming_soon_spec.rb
@@ -1,37 +1,38 @@
-RSpec.describe 'Coming Soon' do
-  subject { page }
+# TODO VS -> Remove
+# RSpec.describe 'Coming Soon' do
+#   subject { page }
 
-  before do
-    allow(Rails).to receive_message_chain(:env, :production?) { true }
-    visit root_path
-  end
+#   before do
+#     allow(Rails).to receive_message_chain(:env, :production?) { true }
+#     visit root_path
+#   end
 
-  context 'regular visitor' do
-    it { is_expected.to have_current_path '/coming-soon' }
+#   context 'regular visitor' do
+#     it { is_expected.to have_current_path '/coming-soon' }
 
-    %w[events talks members chat].each do |path|
-      it "visit /#{path} redirects to /coming-soon path" do
-        visit "/#{path}"
+#     %w[events talks members chat].each do |path|
+#       it "visit /#{path} redirects to /coming-soon path" do
+#         visit "/#{path}"
 
-        is_expected.to have_current_path '/coming-soon'
-      end
-    end
+#         is_expected.to have_current_path '/coming-soon'
+#       end
+#     end
 
-    it 'redirects to login for /admin' do
-      visit '/admin'
-      is_expected.to have_current_path '/users/sign_in'
-    end
-  end
+#     it 'redirects to login for /admin' do
+#       visit '/admin'
+#       is_expected.to have_current_path '/users/sign_in'
+#     end
+#   end
 
-  context 'admin visitor' do
-    before { assume_admin_logged_in }
+#   context 'admin visitor' do
+#     before { assume_admin_logged_in }
 
-    %w[events talks members chat admin].each do |path|
-      it "visit /#{path} redirects to /#{path}" do
-        visit "/#{path}"
+#     %w[events talks members chat admin].each do |path|
+#       it "visit /#{path} redirects to /#{path}" do
+#         visit "/#{path}"
 
-        is_expected.to have_current_path "/#{path}"
-      end
-    end
-  end
-end
+#         is_expected.to have_current_path "/#{path}"
+#       end
+#     end
+#   end
+# end
