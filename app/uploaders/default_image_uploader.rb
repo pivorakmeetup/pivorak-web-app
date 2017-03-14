f6b812efdda1448e5e81bbecb6a43c1e96082d03
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-03-14 21:34:03 +0200

    Change upload dir

--- app/uploaders/default_image_uploader.rb
+++ app/uploaders/default_image_uploader.rb
@@ -11,7 +11,7 @@ class DefaultImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "/uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Process files as they are uploaded:
