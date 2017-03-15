3ad80442b1d1e678fd20e6545508d5156be0b0dd
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-03-15 16:53:19 +0200

    Move email_context to email_template subfolder

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -2,7 +2,7 @@ class EmailTemplate < ApplicationRecord
   validates :body, :subject, :title, presence: true
 
   def render(params = {})
-    context = EmailContext.new(params)
+    context = EmailTemplate::EmailContext.new(params)
     MarkdownRenderer.call context.instance_eval("\"" + body + "\"")
   end
 end
