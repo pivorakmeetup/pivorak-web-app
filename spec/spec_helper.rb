f6f0d4ffa74dd62f5c63facf72c402981bb9eff6
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-03-15 10:20:17 +0200

    Prerelease small fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,6 +51,9 @@ RSpec.configure do |config|
     Warden.test_mode!
   end
 
+  # config.filter_run           focus: true
+  # config.filter_run_excluding skip:  true
+
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
