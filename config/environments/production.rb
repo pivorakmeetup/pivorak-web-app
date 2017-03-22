1863b9bc2c39c3375b694b9166a6f319ac3396da
Denys Medynskyi <denys.medynskyi@gmail.com>
2017-03-22 14:04:36 +0200

    Revert "Use gmail to send emails on production (#242)"
    This reverts commit 288c5628135e30dc2e908d592fe1bf42efe074e1.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,25 +84,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # DEPRECATED due to slow emails
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :authentication => :plain,
-  #   :address => "smtp.gmail.org",
-  #   :port => 587,
-  #   :domain => ENV['MAILGUN_DOMAIN'],
-  #   :user_name => ENV['MAILGUN_USER_NAME'],
-  #   :password => ENV['MAILGUN_PASSWORD']
-  # }
-
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
-    :address => "smtp.gmail.com",
+    :address => "smtp.mailgun.org",
     :port => 587,
-    :domain => ENV['GMAIL_DOMAIN'],
-    :user_name => ENV['GMAIL_USER_NAME'],
-    :password => ENV['GMAIL_PASSWORD'],
-    enable_starttls_auto: true
+    :domain => ENV['MAILGUN_DOMAIN'],
+    :user_name => ENV['MAILGUN_USER_NAME'],
+    :password => ENV['MAILGUN_PASSWORD']
   }
 
 
