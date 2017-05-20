e230748a04489893b9f5a3508eedf1a706082e7f
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-05-20 14:15:36 +0300

    #355 Improve specs

--- .simplecov
+++ .simplecov
@@ -1,14 +1,15 @@
 SimpleCov.profiles.define 'pivorak_cov' do
   add_filter '/spec/'
   add_filter '/config/'
+  add_filter '/lib/tasks'
 
   add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Libraries', 'lib'
-  add_group 'Services', 'app/services'
+  add_group 'Models',      'app/models'
+  add_group 'Helpers',     'app/helpers'
+  add_group 'Libraries',   'lib'
+  add_group 'Services',    'app/services'
   add_group 'Interactors', 'app/interactors'
-  add_group 'Policies', 'app/policies'
+  add_group 'Policies',    'app/policies'
 end
 
 SimpleCov.start 'pivorak_cov'
--- spec/services/talk/fetch_external_video_data_spec.rb
+++ spec/services/talk/fetch_external_video_data_spec.rb
@@ -5,23 +5,42 @@ RSpec.describe Talk::FetchExternalVideoData, vcr: { record: :new_episodes } do
         video_url: 'https://www.youtube.com/watch?v=4QdOwMVMs7k'
   end
 
-  let!(:result) { described_class.call(talk) }
+  describe '.call' do
+    let!(:result) { described_class.call(talk) }
 
-  describe 'updates extra column with external data' do
-    it { expect(talk.extra['video_duration']).to    eq 2864 }
-    it { expect(talk.extra['video_likes_count']).to eq 20 }
-    it { expect(talk.extra['video_views_count']).to eq 458 }
+    context 'valid video_url' do
+      it { expect(talk.extra['video_duration']).to    eq 2864 }
+      it { expect(talk.extra['video_likes_count']).to eq 20 }
+      it { expect(talk.extra['video_views_count']).to eq 458 }
+    end
+
+    context 'no video_url' do
+      let(:talk) { create :talk, video_url: nil }
+
+      it { expect(described_class.call(talk)).to eq nil }
+    end
+
+    context 'invalid video_url' do
+      let(:talk) { create :talk }
+
+      it { expect(described_class.call(talk)).to eq nil }
+    end
   end
 
-  context 'no video_url' do
-    let(:talk) { create :talk, video_url: nil }
+  describe '.fetch_all' do
+    let(:result) { described_class.fetch_all! }
 
-    it { expect(described_class.call(talk)).to eq nil }
-  end
+    let(:talk_2) do
+      create :talk,
+        title: 'Phoenix Framework for the new web by José Valim',
+        video_url: 'https://www.youtube.com/watch?v=Toluc5MCQWs'
+    end
 
-  context 'invalid video_url' do
-    let(:talk) { create :talk }
+    it 'call service for all talks' do
+      expect(Talk::FetchExternalVideoData).to receive(:new).with(talk)
+      expect(Talk::FetchExternalVideoData).to receive(:new).with(talk_2)
 
-    it { expect(described_class.call(talk)).to eq nil }
+      result
+    end
   end
 end
