267bb90836872d3b7fe8f8f69e24c611bd030887
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-05-20 11:15:08 +0300

    #364 Init supporters resource

---
+++ app/controllers/supporters_controller.rb
@@ -0,0 +1,24 @@
+class SupportersController < ApplicationController
+  helper_method :course_supporters, :meetups_supporters, :inlove_partner
+
+  private
+
+  def friends_groups
+    @friends_groups ||= Group.friends
+  end
+
+  def course_supporters
+    @course_supporters ||= friends_groups.find_by(name: 'Course').friends
+  end
+
+  def meetups_supporters
+    @meetups_supporters ||= friends_groups
+      .find_by(name: 'meetups')
+      .friends
+      .where.not(id: inlove_partner.id)
+  end
+
+  def inlove_partner
+    @inlove_partner ||= Friend.find_by(name: 'OnApp')
+  end
+end
