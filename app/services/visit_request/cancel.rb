4a251dde582f8e5350c3fd4f5657b94a75d652c1
avozniak <andrewvoznyak@gmail.com>
2017-07-10 17:23:37 +0300

    Attend first user from wait list

--- app/services/visit_request/cancel.rb
+++ app/services/visit_request/cancel.rb
@@ -6,10 +6,23 @@ class VisitRequest
 
     def call
       visit_request.canceled!
+      attend_first_user_from_waitlist
     end
 
     private
 
     attr_reader :visit_request
+
+    def attend_first_user_from_waitlist
+      if waiting_list_request = VisitRequest.where(waiting_list: true).first
+        waiting_list_request.waiting_list = false
+        waiting_list_request.save
+        if visit_request.event.status == 'registration'
+          VisitRequest::Approve.call(waiting_list_request)
+        else
+          VisitRequest::FinalConfirmation(waiting_list_request)
+        end
+      end
+    end
   end
 end
