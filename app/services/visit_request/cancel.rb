7d6f5f5b568de2f3ad3cd1a178644e1854987a15
avozniak <andrewvoznyak@gmail.com>
2017-07-11 11:17:01 +0300

    Fix method according recomendations

--- app/services/visit_request/cancel.rb
+++ app/services/visit_request/cancel.rb
@@ -14,14 +14,14 @@ class VisitRequest
     attr_reader :visit_request
 
     def attend_first_user_from_waitlist
-      if waiting_list_request = VisitRequest.where(waiting_list: true).first
-        waiting_list_request.waiting_list = false
-        waiting_list_request.save
-        if visit_request.event.status == 'registration'
-          VisitRequest::Approve.call(waiting_list_request)
-        else
-          VisitRequest::FinalConfirmation(waiting_list_request)
-        end
+      return unless waiting_list_request = VisitRequest.waiting_list.first
+
+      waiting_list_request.main_list!
+
+      if visit_request.event.registration?
+        VisitRequest::Approve.call(waiting_list_request)
+      else
+        VisitRequest::FinalConfirmation(waiting_list_request)
       end
     end
   end
