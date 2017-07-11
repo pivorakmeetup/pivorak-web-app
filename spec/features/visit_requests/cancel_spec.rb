d879e7e089fc51d9db1498a42077b8f68ccb3e9f
avozniak <andrewvoznyak@gmail.com>
2017-07-11 08:36:09 +0300

    Add page links tests

--- spec/features/visit_requests/cancel_spec.rb
+++ spec/features/visit_requests/cancel_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe 'Visit Requests CANCEL' do
     visit "/events/#{event.slug}"
   end
 
+  describe 'event page' do
+    it { expect(page).to_not have_link 'Attend' }
+    it { expect(page).to have_link 'Cancel attendance' }
+  end
+
   it 'changes visit request status to canceled' do
     expect { click_link 'Cancel attendance'}.to change{ visit_request.reload.status }.to ('canceled')
     expect(page).to have_content I18n.t('visit_requests.messages.canceled')
