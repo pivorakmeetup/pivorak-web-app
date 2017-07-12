11a7ed91390f424429e453562a84c12672bff4d5
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-07-12 17:07:38 +0300

    #398 Questions CRU (in Season) (#424)

---
+++ components/courses/app/controllers/admin/courses/questions_controller.rb
@@ -0,0 +1,55 @@
+module Admin
+  module Courses
+    class QuestionsController < BaseController
+      helper_method :questions, :question, :current_season
+      add_breadcrumb 'seasons.plural', :admin_courses_seasons_path
+      before_action :add_season_breadcrumb, :add_question_breadcrumb
+      before_action :add_new_breadcump,  only: %i[new create]
+      before_action :add_edit_breadcump, only: %i[edit update]
+
+      def new
+        @question = ::Courses::Question.new
+        render_form
+      end
+
+      def create
+        @question = current_season.questions.new(questions_params)
+        react_to question.save
+      end
+
+      def update
+        react_to question.update(questions_params)
+      end
+
+      private
+
+      def default_redirect
+        redirect_to admin_courses_season_questions_path
+      end
+
+      def question
+        @question ||= ::Courses::Question.find(params[:id])
+      end
+
+      def questions
+        @questions ||= ::Courses::Question.where(season_id: current_season.id)
+      end
+
+      def current_season
+        @current_season ||= ::Courses::Season.friendly.find(params[:season_id])
+      end
+
+      def add_season_breadcrumb
+       add_breadcrumb current_season, path: admin_courses_season_path(current_season)
+      end
+
+      def add_question_breadcrumb
+       add_breadcrumb 'questions.plural', path: admin_courses_season_questions_path(current_season)
+      end
+
+      def questions_params
+        params.require(:question).permit(:body)
+      end
+    end
+  end
+end
---
+++ components/courses/app/services/admin/courses/season/create.rb
@@ -0,0 +1,35 @@
+module Admin
+  module Courses
+    module Season
+      class Create
+        def self.call(*args)
+          new(*args).call
+        end
+
+        def initialize(seasons_params)
+          @seasons_params = seasons_params
+        end
+
+        def call
+          @season = ::Courses::Season.new(seasons_params)
+          default_questions.each do |default|
+            @season.questions.new(body: default)
+          end
+          @season
+        end
+
+        private
+
+        attr_reader :seasons_params
+
+        def default_questions
+          default_questions = [I18n.t('questions.default.first'),
+                               I18n.t('questions.default.second'),
+                               I18n.t('questions.default.third'),
+                               I18n.t('questions.default.fourth'),
+                               I18n.t('questions.default.fifth')]
+        end
+      end
+    end
+  end
+end
