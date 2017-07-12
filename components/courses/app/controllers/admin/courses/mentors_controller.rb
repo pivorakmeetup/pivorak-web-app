daaa6e0a6689bf045b1eb6c32e49cde7ea3e7867
Oleksii Hashparych <lesykgg@gmail.com>
2017-07-12 11:05:35 +0300

    #415: add mentor & current_season helper methods

--- components/courses/app/controllers/admin/courses/mentors_controller.rb
+++ components/courses/app/controllers/admin/courses/mentors_controller.rb
@@ -1,31 +1,29 @@
 module Admin
   module Courses
     class MentorsController < BaseController
-      helper_method :mentors
+      helper_method :mentors, :mentor, :current_season
       add_breadcrumb 'seasons.plural', :admin_courses_seasons_path
       add_breadcrumb 'mentors.plural', :admin_courses_seasons_path
       before_action :add_new_breadcump,  only: %i[new create]
       before_action :add_edit_breadcump, only: %i[edit update]
-      before_action :set_season
 
       def new
-        @mentor = @season.mentors.build
+        @mentor = current_season.mentors.build
         render_form
       end
 
       def create
-        @mentor = @season.mentors.build(mentor_params)
+        @mentor = current_season.mentors.build(mentor_params)
         react_to @mentor.save
       end
 
       def destroy
-        @mentor = ::Courses::Mentor.find(params[:id])
-        react_to @mentor.destroy
+        react_to mentor.destroy
       end
 
       private
 
-      def set_season
+      def current_season
         @season ||= ::Courses::Season.friendly.find(params[:season_id])
       end
 
@@ -34,7 +32,11 @@ module Admin
       end
 
       def mentors
-        @season.mentors
+        current_season.mentors
+      end
+
+      def mentor
+        @mentor ||= ::Courses::Mentor.find(params[:id])
       end
 
       def mentor_params
