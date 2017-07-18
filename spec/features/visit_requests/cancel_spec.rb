fc0bd907a25fc7e4f7fe13ac9fc921ba7c4b72a3
Andrew Vozniak <andrewvoznyak@gmail.com>
2017-07-18 23:39:49 +0300

    #436 Fix tests

--- spec/features/visit_requests/cancel_spec.rb
+++ spec/features/visit_requests/cancel_spec.rb
@@ -1,24 +1,45 @@
 RSpec.describe 'Visit Requests CANCEL' do
-  subject              { click_link 'Cancel attendance'}
   let(:event)          { create(:event) }
   let(:user)           { create(:user)  }
   let!(:visit_request) { create(:visit_request, user: user, event: event) }
 
-  before do
-    assume_logged_in(user)
-    visit "/events/#{event.slug}"
+  context 'when user is not logged in' do
+    before do
+      visit "/events/#{event.id}"
+    end
+
+    describe 'event page' do
+      it { expect(page).to_not have_link 'Attend' }
+      it { expect(page).to have_content 'Please log in or register and then enroll in this event to attend' }
+    end
   end
 
-  it 'changes visit request status to canceled' do
-    expect { click_link 'Cancel attendance'}.to change{ visit_request.reload.status }.to (VisitRequest::CANCELED.to_s)
-    expect(page).to have_content 'Pity.. Hope to see you next time!'
-  end
+  context 'when user is logged in' do
+    before do
+      assume_logged_in(user)
+      visit "/events/#{event.slug}"
+    end
 
-  it 'sends email to admin' do
-    click_link 'Cancel attendance'
-    active_job = active_jobs[0]
-    expect(active_job[:job]).to eq ActionMailer::DeliveryJob
-    expect(active_job[:args][0]).to eq 'VisitRequestMailer'
-    expect(active_job[:args][1]).to eq 'notify_admin_about_canceled_attendee'
+    describe "click 'Cancel attendance'" do
+      before { click_link 'Cancel attendance' }
+
+      it { expect(page).to have_current_path("/") }
+      it { expect(page).to_not have_link 'Attend' }
+
+      it 'changes visit request status to canceled' do
+        expect(visit_request.reload.status).to eq(VisitRequest::CANCELED.to_s)
+      end
+
+      it 'shows flash message' do
+        expect(page).to have_content I18n.t('flash.visit_requests.destroy.success')
+      end
+
+      it 'sends email to admin' do
+        active_job = active_jobs[0]
+        expect(active_job[:job]).to eq ActionMailer::DeliveryJob
+        expect(active_job[:args][0]).to eq 'VisitRequestMailer'
+        expect(active_job[:args][1]).to eq 'notify_admin_about_canceled_attendee'
+      end
+    end
   end
 end
