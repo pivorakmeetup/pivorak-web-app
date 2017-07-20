64db4a2ef61f3ff810d189355d597677ec76a700
Taras Shpachenko <taras.shpachenko@gmail.com>
2017-07-20 19:46:35 +0300

    Event visitors report updates.

--- spec/services/pdf_reports/event_visitors_report_spec.rb
+++ spec/services/pdf_reports/event_visitors_report_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe PdfReports::EventVisitorsReport do
-  let!(:first_user)  { create(:user, first_name: 'First', last_name: 'User') }
-  let!(:second_user)  { create(:user, first_name: 'Second', last_name: 'User') }
-  let!(:third_user)  { create(:user, first_name: 'Third', last_name: 'Long_user_last_name') }
+  let(:first_user)  { create(:user, first_name: 'First', last_name: 'User') }
+  let(:second_user)  { create(:user, first_name: 'Second', last_name: 'User') }
+  let(:third_user)  { create(:user, first_name: 'Third', last_name: 'Long_user_last_name') }
+  let!(:first_visit_request)  { create(:visit_request, user: first_user) }
+  let!(:second_visit_request)  { create(:visit_request, user: second_user) }
+  let!(:third_visit_request)  { create(:visit_request, user: third_user) }
   let(:settings) { { columns: 2, font: :Courier, table: { cell_style: { size: 21 } } } }
-  let(:service) { described_class.new(User.all) }
+  let(:service) { described_class.new(VisitRequest.all) }
   let(:text_inspector) { PDF::Inspector::Text.analyze(service.call.read) }
   let(:page_inspector) { PDF::Inspector::Page.analyze(service.call.read) }
 
