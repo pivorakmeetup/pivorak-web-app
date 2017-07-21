90a745af2819c0594b68ee158c030e7c2e6f38a7
Oksana Svyryda <dev.oksana.svyryda@gmail.com>
2017-07-21 23:33:09 +0300

    405 accesscontrol for test task form (#475)
    * FIX405 access control for test task form
    
    * FIX405 access control for test task form
    
    * FIX405 access control for test task form
    
    * FIX405 access control for test task form

---
+++ components/courses/spec/policies/courses/test_task/create_policy_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Courses::TestTask::CreatePolicy do
+  let(:student) { create (:student) }
+  let(:season)  { create(:season, title: 'Test Season' ) }
+
+  describe '#allowed?' do
+    context 'student has not sent test tasks' do
+      it 'allows to pass policy' do
+        policy = Courses::TestTask::CreatePolicy.new(student, season)
+        expect(policy.allowed?).to be(true)
+      end
+    end
+
+    context 'student has just sent test tasks' do
+      it 'forbids to pass policy' do
+        allow(season).to receive_message_chain(:test_tasks, :exists?).and_return(true)
+        policy = Courses::TestTask::CreatePolicy.new(student, season)
+        expect(policy.allowed?).to be(false)
+      end
+    end
+  end
+end
