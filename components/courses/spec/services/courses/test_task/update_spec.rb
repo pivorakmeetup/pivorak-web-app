50f7fc1392ba51ce30ba589c4aad952157a61aa3
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-07-23 22:27:32 +0300

    Change student status to test_task_done

---
+++ components/courses/spec/services/courses/test_task/update_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Courses::TestTask::Update do
+  let!(:mentor)    { ::Courses::Mentor.create(user_id: 1, season_id: 1) }
+  let!(:student)   { create(:student, season_id: 1, user_id: 1, status: :enrolled) }
+  let!(:test_task) { create(:test_task, student_id: student.id, mentor_id: nil) }
+
+  describe '#call' do
+    it 'updates mentor_id' do
+      Courses::TestTask::Update.call(test_task, mentor)
+
+      expect(test_task.mentor_id).to eq(mentor.id)
+    end
+
+    it 'changes status to test_task_done for student' do
+      Courses::TestTask::Update.call(test_task, mentor)
+
+      expect(test_task.student.status).to eq('test_task_done')
+    end
+  end
+end
