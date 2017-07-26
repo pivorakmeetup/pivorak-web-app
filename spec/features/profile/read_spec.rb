685e1dee735d995cbbe2cc460ae0e17c166a00b7
sontsevolosa <sontsevolosa@gmail.com>
2017-07-26 10:41:32 +0300

    Delete 'Count of sign in' from user profile

--- spec/features/profile/read_spec.rb
+++ spec/features/profile/read_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe 'Profle READ' do
     end
 
     it { expect(page).to have_content 'Tester User' }
-    it { expect(page).to have_content 'Count of sign in' }
     it { expect(page).to have_link 'Edit Profile' }
     it 'redirect to edit path when click Edit' do
       click_link 'Edit Profile'
