a072e931ba2876b02aaf1231a4aa09ca7210141b
Oksana Svyryda <erbe@ukr.net>
2017-07-28 13:38:49 +0300

    test for test task service

--- components/courses/spec/services/courses/test_task/create.rb
+++ components/courses/spec/services/courses/test_task/create.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
 describe Courses::TestTask::Create do
-  let(:test_task_params)       { { git_url: Faker::Internet.url, showcase_url: Faker::Internet.url,
-                         description: Faker::Lorem.sentence } }
-  let(:student_id)   { 1 }
+  let!(:season)          { create(:season, title: 'Test Season') }
+  let!(:user)            { create(:user) }
+  let!(:student)         { ::Courses::Student.create(season_id: 1, user_id: 1, status: :enrolled,
+                                                     personal_info: 'lorem', motivation_info: 'ipsum') }
+  let(:test_task_params) { { git_url: Faker::Internet.url, showcase_url: Faker::Internet.url,
+                             comment: Faker::Lorem.sentence } }
 
   describe '#call' do
     it 'creates test_task' do
-      test_task = described_class.call(test_task_params, student_id)
-      expect { test_task.save }.to change { ::Courses::Homework.count }.by(1)
-    end
-
-    it 'creates homework with correct student_id' do
-      test_task = described_class.call(test_task_params, student_id)
-      test_task.save
-
-      expect(test_task.student_id).to eq(student_id)
+      test_task = described_class.call(test_task_params, student)
+      expect test_task.save
     end
   end
 end
