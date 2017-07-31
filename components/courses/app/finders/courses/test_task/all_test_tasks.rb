c77ebb280d0ea4f6d3edf287a05a0959e0b54726
Oksana Svyryda <dev.oksana.svyryda@gmail.com>
2017-07-31 09:29:30 +0300

    0044 add test task finder (#512)
    * add test_task finder to fix bug with Bullet

---
+++ components/courses/app/finders/courses/test_task/all_test_tasks.rb
@@ -0,0 +1,34 @@
+module Courses
+  class TestTask < ApplicationRecord
+    class AllTestTasks < ApplicationFinder
+      def initialize(season)
+        @season = season
+      end
+
+      def call
+        base_scope = season_test_tasks
+                         .includes(student: :user)
+
+        @test_tasks ||= if exists?
+                          base_scope.includes(mentor: :user)
+                        else
+                          base_scope
+                        end
+      end
+
+      private
+
+      attr_reader :season
+
+      def exists?
+        season_test_tasks.select { |test_task|
+          !test_task.mentor_id.nil?
+        }.any?
+      end
+
+      def season_test_tasks
+        @season_test_tasks ||= season.test_tasks
+      end
+    end
+  end
+end
