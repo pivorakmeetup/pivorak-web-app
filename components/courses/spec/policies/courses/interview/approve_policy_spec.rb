11c0d051fd9752c98d0de90378899feeef02ddc9
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-08-11 10:04:57 +0300

    Fix tests

--- components/courses/spec/policies/courses/interview/approve_policy_spec.rb
+++ components/courses/spec/policies/courses/interview/approve_policy_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe Courses::Interview::ApprovePolicy do
-  let(:season)      { create(:season, title: 'Test Season', status: :selection) }
-  let(:user)        { create(:user) }
-  let!(:mentor)     { ::Courses::Mentor.create(user: user, season: season) }
-  let!(:student)    { create(:student, season: season, user: user, status: :interviewing) }
-  let(:interview)   { create(:interview, mentor: mentor, student: student, status: :completed) }
+  let(:season)          { create(:season, title: 'Test Season', status: :selection) }
+  let(:user)            { create(:user) }
+  let!(:mentor)         { ::Courses::Mentor.create(user: user, season: season) }
+  let!(:student)        { create(:student, season: season, user: user, status: :interviewing) }
+  let(:interview)       { create(:interview, mentor: mentor, student: student, status: :completed) }
 
-  let(:wrong_user)  { create(:user) }
-  let(:w_interview) { create(:interview, mentor: mentor, student: student, status: :pending) }
-  let(:w_student)  { create(:student, season: season, user: wrong_user, status: :graduated) }
+  let(:wrong_user)      { create(:user) }
+  let(:wrong_interview) { create(:interview, mentor: mentor, student: student, status: :pending) }
+  let(:wrong_student)   { create(:student, season: season, user: wrong_user, status: :graduated) }
 
   describe '#allowed?' do
     context 'mentor allowed to approve student after interview' do
@@ -28,14 +28,14 @@ describe Courses::Interview::ApprovePolicy do
 
     context 'interview is not completed' do
       it 'forbids to pass policy' do
-        policy = described_class.new(user, season, w_interview)
+        policy = described_class.new(user, season, wrong_interview)
         expect(policy).not_to be_allowed
       end
     end
 
     context 'status of student is not interviewing' do
       it 'forbids to pass policy' do
-        interview.update(student_id: w_student.id)
+        interview.update(student_id: wrong_student.id)
         interview.reload
 
         policy = described_class.new(user, season, interview)
--- components/courses/spec/policies/courses/lecture/time_policy_spec.rb
+++ components/courses/spec/policies/courses/lecture/time_policy_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 describe Courses::Lecture::TimePolicy do
+  let(:lecture) { build(:lecture) }
+  let(:policy)  { described_class.new(lecture) }
+
   describe '#allowed?' do
     context 'start time is before finish time' do
       it 'allows to pass policy' do
-        lecture = build(:lecture)
-        policy  = described_class.new(lecture)
-
         expect(policy.allowed?).to be_truthy
       end
     end
 
     context 'start time is after finish time' do
       it 'forbids to pass policy' do
-        lecture = build(:lecture, finished_at: (Time.now - 100.days))
-        policy  = described_class.new(lecture)
+        lecture.finished_at = Time.now - 100.days
 
         expect(policy.allowed?).to be_falsey
       end
