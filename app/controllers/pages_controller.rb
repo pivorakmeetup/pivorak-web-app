d902d3594ad16cca853e4d5b6c7597c7742169ba
Kushnir Yuriy <kushnir.yb@gmail.com>
2017-09-15 16:17:37 +0300

    [357] Fixes 404 error for talks#show action (#530)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,9 @@
 class PagesController < ApplicationController
   helper_method :page
 
-  def show
-    page ? render(:show) : not_found
-  end
-
   private
 
   def page
-    @page ||= Page.find_by(url: params[:page_url])
+    @page ||= Page.find_by!(url: params[:page_url])
   end
 end
