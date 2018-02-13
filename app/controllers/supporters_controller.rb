cf7b0834d0ab2ef8895cadc1ea57473fecaddc74
oookli <oklika@gmail.com>
2018-02-13 10:22:13 +0200

    update supporters page

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -1,27 +1,9 @@
 class SupportersController < ApplicationController
-  helper_method :course_supporters, :meetups_supporters, :inlove_partner
+  helper_method :friends
 
   private
 
-  def friends_groups
-    @friends_groups ||= Group.friends
-  end
-
-  def course_supporters
-    @course_supporters ||= friends_groups
-      .find_by(name: 'Course')
-      .friends
-      .where.not(id: inlove_partner.id)
-  end
-
-  def meetups_supporters
-    @meetups_supporters ||= friends_groups
-      .find_by(name: 'meetups')
-      .friends
-      .where.not(id: inlove_partner.id)
-  end
-
-  def inlove_partner
-    @inlove_partner ||= Friend.find_by(name: 'OnApp')
+  def friends
+    @friends ||= Friend.all
   end
 end
