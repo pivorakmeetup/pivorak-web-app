9181a043d4c4d6af34b3e6c83043156437794621
Denys Medynskyi <denys.medynskyi@toptal.com>
2018-05-26 15:05:51 +0300

    Change visitors.pdf representation
    On the event it is not fun to write names by hand.
    I suggest to print everybody names on the paper.
    So PDF should contain names in uppercase.
    
    This PR will change visitors.pdf

--- spec/services/pdf_reports/event_visitors_report_spec.rb
+++ spec/services/pdf_reports/event_visitors_report_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe PdfReports::EventVisitorsReport do
-  let(:first_user)  { create(:user, first_name: 'First', last_name: 'User') }
-  let(:second_user)  { create(:user, first_name: 'Second', last_name: 'User') }
-  let(:third_user)  { create(:user, first_name: 'Third', last_name: 'Long_user_last_name') }
+  let(:first_user)  { create(:user, first_name: 'Alex') }
+  let(:second_user)  { create(:user, first_name: 'Denys') }
+  let(:third_user)  { create(:user, first_name: 'Petro') }
   let!(:first_visit_request)  { create(:visit_request, user: first_user) }
   let!(:second_visit_request)  { create(:visit_request, user: second_user) }
   let!(:third_visit_request)  { create(:visit_request, user: third_user) }
@@ -14,19 +14,18 @@ RSpec.describe PdfReports::EventVisitorsReport do
 
   describe '.call' do
     context 'valid font styles' do
-      it { expect(text_inspector.font_settings.first[:name]).to eq settings[:font] }
-      it { expect(text_inspector.font_settings.first[:size]).to eq settings.dig(:table, :cell_style, :size) }
+      it { expect(text_inspector.font_settings.first[:name]).to eq :Courier }
+      it { expect(text_inspector.font_settings.first[:size]).to eq 21 }
     end
 
     context 'valid text' do
-      it { expect(text_inspector.strings.first).to eq first_user.full_name }
-      it { expect(text_inspector.strings.last).to eq third_user.last_name }
-      it { expect(text_inspector.strings.size).to eq 4 }
+      it { expect(text_inspector.strings.to_a).to eq(['ALEX', 'DENYS', 'PETRO']) }
+      it { expect(text_inspector.strings.size).to eq 3 }
     end
 
     context 'valid pages' do
-      it { expect(page_inspector.pages.first[:strings]).to include(first_user.full_name) }
-      it { expect(page_inspector.pages.second[:strings]).to contain_exactly(third_user.first_name, third_user.last_name) }
+      it { expect(page_inspector.pages.first[:strings]).to include('DENYS') }
+      it { expect(page_inspector.pages.second[:strings]).to include('PETRO') }
       it { expect(page_inspector.pages.size).to eq 2 }
     end
   end
