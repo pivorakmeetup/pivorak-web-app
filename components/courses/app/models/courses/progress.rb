e3889883f436c60b76d3900ebcbedd46d5dbef12
ivanternovyi <ivanternovyi@gmail.com>
2018-06-27 22:19:19 +0300

    Added homework_mark validation for Progress model

--- components/courses/app/models/courses/progress.rb
+++ components/courses/app/models/courses/progress.rb
@@ -6,5 +6,9 @@ module Courses
     belongs_to :lecture
     belongs_to :mentor
     belongs_to :homework
+
+    ALLOWED_MARKS = [nil, -1, 0, 0.5, 1, 1.5, 2]
+
+    validates :homework_mark, inclusion: { in: ALLOWED_MARKS }
   end
 end
