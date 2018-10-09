4e6c01e5b4172723bdae20a8a0d47df33ceb30ea
Denys Medynskyi <denys.medynskyi@toptal.com>
2018-10-09 19:26:38 +0300

    Add groups to supporters page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_142725) do
+ActiveRecord::Schema.define(version: 2018_10_09_104139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -213,6 +213,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_142725) do
     t.string "resource"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "identities", id: :serial, force: :cascade do |t|
