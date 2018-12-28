fe7d482b7f05c29db78d910b93bf0f0dcd9af362
Denys Medynskyi <denys.medynskyi@toptal.com>
2018-12-28 13:05:36 +0200

    Hide donate feature

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -1,4 +1,7 @@
 class DonateController < ApplicationController
+  # Disabled because of tax thing.
+  # Ask at Slack #core-team if it can be enabled.
+  before_action :redirect_home
   helper_method :form, :locale
   layout 'devise'
 
@@ -16,6 +19,10 @@ class DonateController < ApplicationController
   end
 
   private
+
+  def redirect_home
+    redirect_to root_path
+  end
 
   def locale
     # hardcoded for now
