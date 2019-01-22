0f7acd659719ccb01c83e94db2b3a5ca82bf5d77
Denys Medynskyi <denys.medynskyi@toptal.com>
2019-01-22 11:27:31 +0200

    Revert "Hide donate feature (#687)"
    This reverts commit 0f08914b18fb89063ab4c99a649428bd5b12ae2a.

--- spec/features/donate/create_spec.rb
+++ spec/features/donate/create_spec.rb
@@ -1,14 +1,10 @@
 RSpec.describe 'Create Donation' do
-  pending 'Disabled because of tax thing'
-
   subject { page }
 
-  before do
-    visit donate_path
-  end
+  before { visit donate_path }
 
   context 'when terms of use are accepted' do
-    xit 'toggles submit and payment button when checkbox is clicked' do
+    it 'toggles submit and payment button when checkbox is clicked' do
       expect(page).to have_selector('.js-donate-form-payment-btn', visible: true)
       expect(page).to have_selector('.js-donate-form-validation-btn', visible: false)
 
@@ -20,7 +16,7 @@ RSpec.describe 'Create Donation' do
   end
 
   context 'when terms of use not accepted' do
-    xit 'does not proceed with payment' do
+    it 'does not proceed with payment' do
       click_button I18n.t('pages.donate.submit_btn', locale: :ua)
 
       expect(page).to have_content I18n.t('pages.donate.failure', locale: :ua)
--- spec/features/donations/donation_spec.rb
+++ spec/features/donations/donation_spec.rb
@@ -1,10 +1,9 @@
 RSpec.describe 'Donate Process' do
-  pending 'Disabled because of tax thing'
   subject { page }
 
   before { visit root_path }
 
-  xit 'home page should have donate link' do
+  it 'home page should have donate link' do
     expect(page).to have_selector('a', text: 'Donate')
   end
 end
