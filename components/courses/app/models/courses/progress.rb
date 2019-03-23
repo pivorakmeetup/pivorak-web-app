facc320d7dd7953b1a800be67f9ca2d0e626123c
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-03-23 20:16:00 +0200

    Validate homework mark (#592)
    * Added homework_mark validation for Progress model
    
    * Mark buttons generating based on ALLOWED_MARKS
    
    * Used .compact instead of .drop(1) to skip nil element
    
    * Tests for validations
    
    * Test: buttons for mark set
    
    * Added tests for validation in Progress model
    
    * Moved validation tests undel models/ directory
    
    * Added allow_blank option

--- components/courses/app/models/courses/progress.rb
+++ components/courses/app/models/courses/progress.rb
@@ -6,5 +6,9 @@ module Courses
     belongs_to :lecture
     belongs_to :mentor
     belongs_to :homework
+
+    ALLOWED_MARKS = [-1, 0, 0.5, 1, 1.5, 2]
+
+    validates :homework_mark, inclusion: { in: ALLOWED_MARKS }, allow_blank: true
   end
 end
