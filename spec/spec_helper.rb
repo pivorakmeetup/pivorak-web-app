34e597131bf6a5fafeb3249a596748e814c95a76
bogdanvlviv <bogdanvlviv@gmail.com>
2019-09-02 21:14:03 +0300

    Add Docker setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,6 +55,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
+  DatabaseCleaner.allow_remote_database_url = true
+
   config.before(:suite) do |_|
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
