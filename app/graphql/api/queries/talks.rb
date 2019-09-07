2d674219cc4aa3b4de0bbfee313ac334b1427cb0
aYaremchuk <jaremchuk.andrij@gmail.com>
2019-09-07 15:37:49 +0300

    Add filtering talks by tags

--- app/graphql/api/queries/talks.rb
+++ app/graphql/api/queries/talks.rb
@@ -6,17 +6,19 @@ module Api
       type [Types::TalkType], null: false
 
       argument :pagination, InputObjects::Pagination, required: false
-      argument :search, InputObjects::Search,  required: false
+      argument :search, InputObjects::Search, required: false
+      argument :filter, InputObjects::Filter, required: false
 
       description "Get list of all visible pivorak's talks info"
 
-      def resolve(pagination: {}, search: {})
+      def resolve(pagination: {}, search: {}, filter: {})
         scope = if search.present?
                   Search::Resource.call({ query: search['title'] }.merge(model: Talk))
                 else
                   Talk.all
                 end
 
+        scope = scope.tagged_with(filter[:tags]) if filter[:tags]
         scope = scope.sorted.offset(pagination[:offset]).limit(pagination[:limit]) if pagination.present?
         scope
       end
