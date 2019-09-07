dff9b826e2fdf2dd572fc5ee7f9e1cafe5ed400e
Andrew Shyika <andrew.shyika@gmail.com>
2019-09-07 15:42:26 +0300

    Specs, specs, they can not be enough

--- spec/graphql/queries/events_task_spec.rb
+++ spec/graphql/queries/events_task_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe 'GraphqQL Api Events query' do
   let(:result)    { Api::Schema.execute(query, context: context, variables: variables) }
   let(:user)      { build_stubbed(:user) }
   let(:context)   { { current_user: user } }
-  let(:variables) { {} }
+
+  let(:variables) { { filter: { status: status, venueIds: venue_ids } } }
+  let(:venue_ids) { [venue_b.id] }
+  let(:status)    { 'PASSED' }
 
   context 'all events' do
     let(:query) do
@@ -23,23 +26,56 @@ RSpec.describe 'GraphqQL Api Events query' do
       GQL
     end
 
-    let(:variables) { { filter: { status: status, venueIds: venue_ids } } }
-    let(:venue_ids) { [venue_b.id] }
-    let(:status)    { 'PASSED' }
-
-    let!(:event_a) { create(:event, status: :passed,  venue_id: venue_a.id) }
-    let!(:event_b) { create(:event, status: :passed,  venue_id: venue_b.id) }
-    let!(:event_c) { create(:event, status: :planned, venue_id: venue_c.id) }
-
     let!(:venue_a) { create(:venue) }
     let!(:venue_b) { create(:venue) }
     let!(:venue_c) { create(:venue) }
 
-    it 'returns lits of passed events with title and venues' do
-      expect(result['data']['events'].size).to                eq 1
-      expect(result['data']['events'][0]['id']).to            eq event_b.id.to_s
-      expect(result['data']['events'][0]['title']).to         eq event_b.title
-      expect(result['data']['events'][0]['venue']['name']).to eq event_b.venue.name
+    context 'events filter by venues' do
+      context 'when there is one event in one venue' do
+        let!(:event_a) { create(:event, status: :passed,  venue_id: venue_a.id) }
+        let!(:event_b) { create(:event, status: :passed,  venue_id: venue_b.id) }
+        let!(:event_c) { create(:event, status: :planned, venue_id: venue_c.id) }
+
+        it 'returns lits of passed events with title and venues' do
+          expect(result['data']['events'].size).to                eq 1
+          expect(result['data']['events'][0]['id']).to            eq event_b.id.to_s
+          expect(result['data']['events'][0]['title']).to         eq event_b.title
+          expect(result['data']['events'][0]['venue']['name']).to eq event_b.venue.name
+        end
+      end
+
+      context 'when there is two events in one venue' do
+        let!(:event_a) { create(:event, status: :passed,  venue_id: venue_b.id) }
+        let!(:event_b) { create(:event, status: :passed,  venue_id: venue_b.id) }
+        let!(:event_c) { create(:event, status: :planned, venue_id: venue_c.id) }
+
+        it 'returns lits of passed events with title and venues' do
+          expect(result['data']['events'].size).to                eq 2
+          expect(result['data']['events'][0]['id']).to            eq event_b.id.to_s
+          expect(result['data']['events'][0]['title']).to         eq event_b.title
+          expect(result['data']['events'][0]['venue']['name']).to eq event_b.venue.name
+        end
+      end
+
+      context 'when there is two events in two venues' do
+        let!(:event_a) { create(:event, status: :passed,  venue_id: venue_a.id) }
+        let!(:event_b) { create(:event, status: :passed,  venue_id: venue_b.id) }
+        let!(:event_c) { create(:event, status: :planned, venue_id: venue_c.id) }
+
+        let(:venue_ids) { [venue_b.id, venue_a.id] }
+
+        it 'returns lits of passed events with title and venues' do
+          expect(result['data']['events'].size).to                eq 2
+
+          expect(result['data']['events'][0]['id']).to            eq event_b.id.to_s
+          expect(result['data']['events'][0]['title']).to         eq event_b.title
+          expect(result['data']['events'][0]['venue']['name']).to eq event_b.venue.name
+
+          expect(result['data']['events'][1]['id']).to            eq event_a.id.to_s
+          expect(result['data']['events'][1]['title']).to         eq event_a.title
+          expect(result['data']['events'][1]['venue']['name']).to eq event_a.venue.name
+        end
+      end
     end
   end
 end
