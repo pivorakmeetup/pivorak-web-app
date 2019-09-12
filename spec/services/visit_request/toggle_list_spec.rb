a1ab5c32bdde8244af79df62182450378a337d4c
Denys Medynskyi <denys.medynskyi@gmail.com>
2019-09-12 05:26:47 +0200

    Send confirmation email after user moved to main list (#813)

---
+++ spec/services/visit_request/toggle_list_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe VisitRequest::ToggleList do
+  subject(:call) { described_class.call(visit_request) }
+
+  context 'when request is in waiting list' do
+    let(:visit_request) { create(:visit_request, waiting_list: true) }
+
+    it 'changes list to main' do
+      expect { call }.to change(visit_request.reload, :waiting_list).from(true).to(false)
+    end
+
+    it 'enques needs confirmation' do
+      call
+
+      active_job = active_jobs[0]
+      expect(active_job[:job]).to eq ActionMailer::DeliveryJob
+      expect(active_job[:args][0]).to eq 'VisitRequestMailer'
+      expect(active_job[:args][1]).to eq 'confirmation'
+    end
+  end
+
+  context 'when request is in main list' do
+    let(:visit_request) { create(:visit_request, waiting_list: false) }
+
+    it 'changes list to waiting' do
+      expect { call }.to change(visit_request.reload, :waiting_list).from(false).to(true)
+    end
+  end
+end
