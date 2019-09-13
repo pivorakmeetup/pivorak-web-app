6917771d0899b08ae4a6289d5d5d0df1f065cad3
Denis Pankratov <dip00dip@gmail.com>
2019-09-13 10:54:42 +0300

    GraphQL API: date range filter for Events (#810)

--- app/graphql/api/input_objects/event_filter.rb
+++ app/graphql/api/input_objects/event_filter.rb
@@ -5,6 +5,7 @@ module Api
     class EventFilter < BaseInputObject
       argument :status,    [Enums::EventStatus], 'Filter events by status', required: false
       argument :venue_ids, [Integer], 'Filter events by venue ids', required: false
+      argument :start_at,  EventRange, 'Filter events by started_at', required: false
     end
   end
 end
--- app/graphql/api/queries/events.rb
+++ app/graphql/api/queries/events.rb
@@ -10,14 +10,23 @@ module Api
 
       description "Get list of all visible pivorak's events info"
 
+      # rubocop:disable Metrics/AbcSize
       def resolve(pagination: {}, filter: {})
         events = Event.visible.published
 
         events = events.where(status:   filter[:status])   if filter[:status]
         events = events.where(venue_id: filter[:venueIds]) if filter[:venueIds]
 
+        if filter[:start_at].present?
+          start_from = filter.dig(:start_at, :from) || 10.years.ago
+          end_at = filter.dig(:start_at, :to) || Time.current
+
+          events = events.where(started_at: start_from.utc..end_at.utc)
+        end
+
         events.offset(pagination[:offset]).limit(pagination[:limit]).order(started_at: :desc)
       end
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
