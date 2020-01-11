6bc37d1605437fc75ac57d7bc70059b77ef11d08
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-01-11 10:11:21 +0200

    [C-868] Remove Goals feature :fire: [STEP-1] Revert bundler version [STEP-2] Drop Goal from database [STEP-3] Adjust specs/features

--- app/helpers/admin/navigation_helper.rb
+++ app/helpers/admin/navigation_helper.rb
@@ -28,10 +28,6 @@ module Admin
       link_to t('talks.plural'), admin_talks_path, class: class_for(:talks)
     end
 
-    def admin_goals_link
-      link_to t('goals.plural'), admin_goals_path, class: class_for(:goals)
-    end
-
     def admin_members_link
       link_to t('members.plural'), admin_members_path, class: class_for(:members)
     end
