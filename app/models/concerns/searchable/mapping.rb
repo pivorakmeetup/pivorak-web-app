53114e9ad17a06c7dee5bd8687693a0fd24a2ce9
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-01-12 00:11:14 +0200

    [STEP-2] Remove unused features: Donations & Goals 🔥
    * [C-870] Remove Donation
    [STEP-1] Remove Donation related code
    [STEP-2] Drop database table
    [STEP-3] Remove specs
    [STEP-4] Fix specs
    
    * [C-868] Remove Goals feature :fire:
    [STEP-1] Revert bundler version
    [STEP-2] Drop Goal from database
    [STEP-3] Adjust specs/features

--- app/models/concerns/searchable/mapping.rb
+++ app/models/concerns/searchable/mapping.rb
@@ -6,7 +6,6 @@ module Searchable
       user:          Configuration::User,
       event:         Configuration::Event,
       group:         Configuration::Group,
-      goal:          Configuration::Goal,
       talk:          Configuration::Talk,
       venue:         Configuration::Venue,
       page:          Configuration::Page,
