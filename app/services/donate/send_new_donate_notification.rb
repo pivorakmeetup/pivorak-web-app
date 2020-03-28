6306c0427249d0265884b3bcba1024e036dc1654
Bohdan V <g3dinua@gmail.com>
2020-03-29 00:25:59 +0200

    Do not notify multiple times for same donation

--- app/services/donate/send_new_donate_notification.rb
+++ app/services/donate/send_new_donate_notification.rb
@@ -3,15 +3,21 @@
 # Donate::SendNewDonateNotification.call('amount' => 5, 'currency' => 'USD')
 module Donate
   class SendNewDonateNotification < ApplicationService
-    attr_reader :amount, :currency
+    attr_reader :amount, :currency, :order_reference
 
     def initialize(params)
       @amount = params.fetch(:amount)
       @currency = params.fetch(:currency)
+      # TODO: Add apropriate fix with DB storage for orderReference
+      Rails.logger.info params.inspect
+      @order_reference = params.fetch(:orderReference)
+      Thread.current[:donations] ||= []
     end
 
     def call
       return if amount.zero?
+      return if Thread.current[:donations].include?(order_reference)
+      Thread.current[:donations].push(order_reference)
 
       SlackNotifier.call(
         channel: ENV.fetch('SLACK_NEW_DONATION_CHANNEL'),
